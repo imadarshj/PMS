64945bdb78952792c9de57bcb4e05daf31469e49
tushar-singh-89 <tmanu.ts@gmail.com>
2021-05-06 22:17:49 +0530

    Added ML model

--- Web App/app/forms.py
+++ Web App/app/forms.py
@@ -3,34 +3,39 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 import email_validator
 from app.models import User
+import pickle
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField('UserName', 
-        validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('UserName',
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-        validators=[DataRequired()])
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
     confirmPassword = PasswordField('Confirm Password',
-        validators=[DataRequired(), EqualTo('password')])
-    
+                                    validators=[DataRequired(), EqualTo('password')])
+
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose an another username')
+            raise ValidationError(
+                'That username is taken. Please choose an another username')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose an another email')
+            raise ValidationError(
+                'That email is taken. Please choose an another email')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-        validators=[DataRequired()])
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
--- Web App/app/routes.py
+++ Web App/app/routes.py
@@ -6,6 +6,15 @@ from app.forms import RegistrationForm, LoginForm
 from app.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
+from sklearn.preprocessing import StandardScaler
+import pickle
+import pandas as pd
+import numpy as np
+
+model = pickle.load(open(
+    'C:\\Users\\Tushar\\Downloads\\TARP-main\\TARP-main\\Web App\\app\\model.pkl', 'rb'))
+
+
 def datetime_range(start, end, delta):
     current = start
     i = 0
@@ -20,9 +29,11 @@ def datetime_range(start, end, delta):
 def home():
     return render_template('home.html', title='home')
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -30,13 +41,17 @@ def register():
         return redirect(url_for('home'))
     forms = RegistrationForm()
     if forms.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(forms.password.data).decode('utf-8')
-        user = User(username=forms.username.data, email=forms.email.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(
+            forms.password.data).decode('utf-8')
+        user = User(username=forms.username.data,
+                    email=forms.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f'Account created for {forms.username.data}! You are now able to login', 'success')
+        flash(
+            f'Account created for {forms.username.data}! You are now able to login', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=forms)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -53,25 +68,28 @@ def login():
             flash('Login unsuccessful. Please check your email and password.', 'danger')
     return render_template('login.html', title='Login', form=forms)
 
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
 @app.route("/dashboard")
 @login_required
 def dashboard():
     # text = request.args.get('jsdata')
-    f = open('D:\\TARP\Web App\\app\\input.txt', 'r')
+    f = open(
+        'C:\\Users\\Tushar\\Downloads\\TARP-main\\TARP-main\\Web App\\app\\input.txt', 'r')
     data = []
     bpm = []
     temperature = []
     time = []
-	
+
     while(1):
         try:
-            # ser = serial.Serial('/dev/rfcomm1',9600) 
-	 		# input_list.append(str(ser.readline()))
+            # ser = serial.Serial('/dev/rfcomm1',9600)
+            # input_list.append(str(ser.readline()))
             for line in f:
                 data.append(line.strip())
             data = [i for i in data if i != '']
@@ -79,7 +97,18 @@ def dashboard():
                 temp = item.split(' ')
                 bpm.append(temp[1])
                 temperature.append(temp[3])
-            time = [dt.strftime('%Y-%m-%dT%H:%M') for dt in datetime_range(datetime.now(), len(data), timedelta(minutes=15))]
+            time = [dt.strftime('%Y-%m-%dT%H:%M') for dt in datetime_range(
+                datetime.now(), len(data), timedelta(minutes=15))]
             return render_template('dashboard.html', title='Dashboard', bpm=bpm, temperature=temperature, time=time)
         except:
             break
+
+
+@app.route('/predict')
+def predict():
+    check = pd.DataFrame({'age': [67],	'sex': [1],	'cp': [2],	'trestbps': [152],	'chol': [212],	'fbs': [
+                         0],	'restecg': [0], 'thalach': [150],	'exang': [0],	'oldpeak': [0.8],	'slope': [1],	'ca': [0],	'thal': [3]})
+    scaler = StandardScaler()
+    check = scaler.fit_transform(check)
+    output = model.predict(check)
+    return render_template('predict.html', prediction_text='the output is {}'.format(output[0]), title='predict')
